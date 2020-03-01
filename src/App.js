622be36e53e8978917848652589f69ff4b7c0a55
unknown <gebb.freelancer@gmail.com>
2020-03-01 15:16:55 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
+import NavigationHeader from "./components/NavigationHeader";
+import WelcomeHeader from "./components/WelcomeHeader";
+import About from "./components/About";
+import Team from "./components/Team";
+import ContactInfo from "./components/ContactInfo";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const portfolioLinks = [
+    {
+      title: "Threads",
+      caption: "Illustration"
+    },
+    {
+      title: "Explore",
+      caption: "Graphic Design"
+    },
+    {
+      title: "Finish",
+      caption: "Identity"
+    },
+    {
+      title: "Lines",
+      caption: "Branding"
+    },
+    {
+      title: "Southwest",
+      caption: "Website Design"
+    },
+    {
+      title: "Window",
+      caption: "Photography"
+    },
+    {
+      title: "Pizza",
+      caption: "I love pizza!"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <NavigationHeader></NavigationHeader>
+      <WelcomeHeader></WelcomeHeader>
+      <Services></Services>
+      <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+      <About></About>
+      <Team></Team>
+
+      <ContactInfo></ContactInfo>
+      <Footer></Footer>
     </div>
   );
 }
